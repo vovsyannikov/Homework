425b0519d919647ad947b67adfd6287417854037
Виталий <its.vint@ya.ru>
2020-08-23 21:10:10 +0300

    Initial Commit

---
+++ Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Виталий Овсянников on 23.08.2020.
+//  Copyright © 2020 Виталий Овсянников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

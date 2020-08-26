2cbc8a5c69d0d9ed732d8b6162fdae4745eee112
Виталий <its.vint@ya.ru>
2020-08-26 22:09:18 +0300

    Наметки вида

--- Homework/FoodDeliveryViewController.swift
+++ Homework/FoodDeliveryViewController.swift
@@ -8,30 +8,49 @@
 
 import UIKit
 
+enum FoodType{
+    case Burger
+    case Pizza
+    case Sushi
+}
+
+enum Star: String {
+    case empty = "star"
+    case halfFull = "star.lefthalf.fill"
+    case full = "star.fill"
+}
+
 class Restaraunt{
     var restName = ""
     var rating = 0.0
+    let stars: Dictionary<Star, UIImage> = [
+        .empty   :UIImage(systemName: Star.empty.rawValue)!,
+        .halfFull:UIImage(systemName: Star.halfFull.rawValue)!,
+        .full:UIImage(systemName: Star.full.rawValue)!
+    ]
     
-    func setRest(restName: String, rating: Double){
-        self.restName = restName
-        self.rating = rating
-    }
+//    func getVisualRating() -> UIView{
+//        var ratingVisual = UIView(frame: CGRect(x: 0, y: 0, width: 120, height: 80))
+//        var tempRating = rating
+//
+//        for _ in 1...5{
+//            if tempRating.rounded() > 0{
+//                tempRating -= 1
+//                ratingVisual.addSubview(UIImageView(image: stars[.full]))
+//            } else if tempRating > 0{
+//
+//            }
+//        }
+//
+//        return ratingVisual
+//    }
 }
-
 class FoodItem: Restaraunt{
     var image = UIImage.init()
     var name = ""
     var description = ""
-    var components: [(checkbox: UIImage, ingr: String)] = []
-    
-    func makeFood(restName: String, restRating: Double, image: UIImage, name: String, description: String, components: [(checkbox: UIImage, ingr: String)]) {
-        
-        setRest(restName: restName, rating: restRating)
-        self.image = image
-        self.name = name
-        self.description = description
-        self.components = components
-    }
+    var components: [String] = []
+    var type = FoodType.Burger
 }
 
 class FoodDeliveryViewController: UIViewController {
@@ -40,10 +59,31 @@ class FoodDeliveryViewController: UIViewController {
     
     var food = [FoodItem()]
     
+    func makeSomeFood(){
+        
+        for _ in 1...3 {
+            food.append(FoodItem())
+        }
+        
+        food[0].restName = "Texas burgers"
+        food[0].rating = 4.5
+        
+        food[0].name = "Техасский оригинальный бургер"
+        food[0].description = "Рецепт этого сочного бургера  привезен прямиком из южных широт США. Пропитанный запахом свободы и открытого огня. В сочетании с нашим фирменным соусом \"Техас\" этот бургер превращается в часть искусства, которое вы не можете пропустить!"
+        food[0].components.append(contentsOf: ["Говядина", "Соленые огурчики", "Сыр Чеддар", "Помидор", "Салат", "Булочки", "Фирменный соус \"Техас\""])
+        
+        let cell = UIView(frame: CGRect(x: 0, y: 25, width: view.frame.size.width, height: 100))
+        var nameLabel = UILabel.init()
+        nameLabel.text = food[0].name
+        nameLabel.frame = CGRect(x: cell.bounds.minX + 5, y: cell.bounds.minY + 5, width: 150, height: 80)
+        cell.addSubview(nameLabel)
+        foodScrollView.addSubview(cell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        makeSomeFood()
+        foodScrollView.contentSize = CGSize(width: view.frame.size.width, height: CGFloat(food.count * 100))
     }
 
 }

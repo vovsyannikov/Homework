f01013445472fa71939cb684a0e7badeaef0fd22
Виталий <its.vint@ya.ru>
2020-09-02 20:37:59 +0300

    Решена ошибка перезаполнения SegmentControl

--- Homework/SegmentsView.swift
+++ Homework/SegmentsView.swift
@@ -26,24 +26,28 @@ class SegmentsView: UIView {
     @IBInspectable var backColor: UIColor = UIColor.systemGray6
     
     func createSegmentView(){
+        
         segmentView.insertSegment(withTitle: firstTitle, at: 0, animated: false)
         segmentView.insertSegment(withTitle: secondTitle, at: 1, animated: false)
         segmentView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
         segmentView.selectedSegmentIndex = 0
         segmentView.selectedSegmentTintColor = selectedSegmentColor
         segmentView.backgroundColor = backColor
+        segmentView.contentMode = UIView.ContentMode.scaleAspectFit
+        
+        print(segmentView.numberOfSegments)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        createSegmentView()
         
         delegate?.getSelectedSegment(segmentView.selectedSegmentIndex)
         
         if isDrawn{return}
         isDrawn = true
         
+        createSegmentView()
         addSubview(segmentView)
         
     }

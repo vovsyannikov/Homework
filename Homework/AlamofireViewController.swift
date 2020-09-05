55923d9856de458036ee9d68d87e6dee54675640
Виталий <its.vint@ya.ru>
2020-09-05 22:07:13 +0300

    Созданы необходимые файлы

---
+++ Homework/AlamofireViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AlamofireViewController.swift
+//  Homework
+//
+//  Created by Виталий Овсянников on 05.09.2020.
+//  Copyright © 2020 Виталий Овсянников. All rights reserved.
+//
+
+import UIKit
+
+class AlamofireViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+}
---
+++ Homework/StandartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StandartViewController.swift
+//  Homework
+//
+//  Created by Виталий Овсянников on 05.09.2020.
+//  Copyright © 2020 Виталий Овсянников. All rights reserved.
+//
+
+import UIKit
+
+class StandartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}

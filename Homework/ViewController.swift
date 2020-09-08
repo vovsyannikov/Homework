fe80842bcbb6fe250237ece73fee073e077a5c0c
Виталий <its.vint@ya.ru>
2020-09-09 00:28:31 +0300

    Готова верстка задания

--- Homework/ViewController.swift
+++ Homework/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Anim{
+    let names = [
+        "Фон",
+        "Перемещение",
+        "Закругление краев",
+        "Поворот",
+        "Исчезновение",
+        "Увеличение",
+        "∞ поворот"
+    ]
+}
 
+class ViewController: UIViewController {
+    
+    var currentAnim = 0
+
+    @IBOutlet weak var testSubjectView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var anim = Anim()
+        
+        nameLabel.text = anim.names[currentAnim]
+        numberLabel.text = "\(currentAnim + 1)"
     }
 
-
+    @IBAction func nextAnimation(_ sender: Any) {
+        if currentAnim == Anim().names.count - 1{
+            currentAnim = 0
+        } else {
+            currentAnim += 1
+        }
+        viewDidLoad()
+    }
+    @IBAction func previousAnimation(_ sender: Any) {
+        if currentAnim == 0{
+            currentAnim = Anim().names.count - 1
+        } else {
+            currentAnim -= 1
+        }
+        viewDidLoad()
+    }
+    @IBAction func playAnimation(_ sender: Any) {
+    }
+    
 }
 
